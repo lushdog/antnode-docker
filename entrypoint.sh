12d5bfb1711f93fd64c795ed53bbb89d95d9347f
HoraceZhang <impressioncr@hotmail.com>
2025-02-17 01:48:53 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ for ((COUNT=1; COUNT<=NODE_COUNT; COUNT++)); do
     echo "start $COUNT node"
     mkdir -p /root/antnode_data/node_$COUNT
     ./antnode --rewards-address $REWARD_ADDRESS --root-dir /root/antnode_data/node_$COUNT evm-arbitrum-one &
-    sleep 0.5
+    sleep 0.1
 done
 
 wait
