ecd7c04075aa5c63858d3ff1fa2b9170143336e4
HoraceZhang <impressioncr@hotmail.com>
2025-02-16 14:08:17 +0800

    fix for

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ NODE_COUNT=${NODE_COUNT:-"5"}
 
 REWARD_ADDRESS=${REWARD_ADDRESS}
 
-for ((COUNT=1; CORE<=NODE_COUNT; COUNT++)); do
+for ((COUNT=1; COUNT<=NODE_COUNT; COUNT++)); do
     echo "start $COUNT node"
     ./antnode --rewards-address $REWARD_ADDRESS --root-dir /root/antnode_data/node_$COUNT evm-arbitrum-one &
 done
